afd59ded6d85a0f3d6bcdd14490ed58d839ad980
Rob Miracle <rob@coronalabs.com>
2015-06-27 16:44:43 -0400

    Better commented the module, fixed a small bug
    Heavily commented the game.lua module.
    Removed param since I wasn't using them
    Fixed the background to center instead of anchoring the topLeft corner
    to 0, 0 since that doesn't go with how we are doing config.lua

--- game.lua
+++ game.lua
@@ -12,16 +12,21 @@ local myData = require( "mydata" )
 -- 
 -- define local variables here
 --
-local params
-local currentScore
-local currentScoreDisplay
-local levelText
-local spawnTimer
+local currentScore          -- used to hold the numeric value of the current score
+local currentScoreDisplay   -- will be a display.newText() that draws the score on the screen
+local levelText             -- will be a display.newText() to let you know what level you're on
+local spawnTimer            -- will be used to hold the timer for the spawning engine
 
 --
 -- define local functions here
 --
 local function handleWin( event )
+    --
+    -- When you tap the "I Win" button, reset the "nextlevel" scene, then goto it.
+    --
+    -- Using a button to go to the nextlevel screen isn't realistic, but however you determine to 
+    -- when the level was successfully beaten, the code below shows you how to call the gameover scene.
+    --
     if event.phase == "ended" then
         composer.removeScene("nextlevel")
         composer.gotoScene("nextlevel", { time= 500, effect = "crossFade" })
@@ -30,6 +35,12 @@ local function handleWin( event )
 end
 
 local function handleLoss( event )
+    --
+    -- When you tap the "I Loose" button, reset the "gameover" scene, then goto it.
+    --
+    -- Using a button to end the game isn't realistic, but however you determine to 
+    -- end the game, the code below shows you how to call the gameover scene.
+    --
     if event.phase == "ended" then
         composer.removeScene("gameover")
         composer.gotoScene("gameover", { time= 500, effect = "crossFade" })
@@ -38,6 +49,12 @@ local function handleLoss( event )
 end
 
 local function handleEnemyTouch( event )
+    --
+    -- When you touch the enemy:
+    --    1. Increment the score
+    --    2. Update the onscreen text that shows the score
+    --    3. Kill the object touched
+    --
     if event.phase == "began" then
         currentScore = currentScore + 10
         currentScoreDisplay.text = string.format( "%06d", currentScore )
@@ -56,38 +73,93 @@ local function spawnEnemy( )
     local x = math.random(50, display.contentCenterX - 50)
     local enemy = display.newCircle(x, -50, 25)
     enemy:setFillColor( 1, 0, 0 )
+    -- 
+    -- must be inserted into the the group to be managed
+    --
     sceneGroup:insert( enemy )
+    --
+    -- Add the physics body and the touch handler
+    --
     physics.addBody( enemy, "dynamic", { radius = 25 } )
+    --
+    -- Since the touch handler is on an "object" and not the whole screen, 
+    -- you don't need to remove it. When Composer hides the scene, it can't be
+    -- interacted with and doesn't need removed. 
+    -- when the scene is destroyed any display objects will be removed and that
+    -- will remove this listener.
     enemy:addEventListener( "touch", handleEnemyTouch )
+
+    --
+    -- Not needed in this implementation, but you may want to call spawnEnemy() to create one 
+    -- and you might want to pass that enemy back to the caller.
     return enemy
 end
 
 local function spawnEnemies()
+    --
+    -- Spawn a new enemy every second until canceled.
+    -- spawnTimer holds the handle to the timer so we can cancel it later later.
+    --
     spawnTimer = timer.performWithDelay( 1000, spawnEnemy, -1 )
 end
 
+--
+-- This function gets called when composer.gotoScene() gets called an either:
+--    a) the scene has never been visited before or
+--    b) you called composer.removeScene() or composer.removeHidden() from some other
+--       scene.  It's possible (and desirable in many cases) to call this once, but 
+--       show it multiple times.
+--
 function scene:create( event )
+    --
+    -- self in this case is "scene", the scene object for this level. 
+    -- Make a local copy of the scene's "view group" and call it "sceneGroup". 
+    -- This is where you must insert everything (display.* objects only) that you want
+    -- Composer to manage for you.
     local sceneGroup = self.view
 
-    params = event.params
-
+    -- 
+    -- You need to start the physics engine to be able to add objects to it, but...
+    --
     physics.start()
+    --
+    -- because the scene is off screen being created, we don't want the simulation doing
+    -- anything yet, so pause it for now.
+    --
     physics.pause()
 
+    --
+    -- make a copy of the current level value out of our
+    -- non-Global app wide storage table.
+    --
     local thisLevel = myData.settings.currentLevel
+
+    --
+    -- create your objects here
+    --
     
     --
     -- These pieces of the app only need created.  We won't be accessing them any where else
-    local background = display.newRect(0,0, display.contentWidth, display.contentHeight)
-    background.anchorX = 0
-    background.anchorY = 0
+    -- so it's okay to make it "local" here
+    --
+    local background = display.newRect(display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
     background:setFillColor( 0.6, 0.7, 0.3 )
+    --
+    -- Insert it into the scene to be managed by Composer
+    --
     sceneGroup:insert(background)
 
+    --
+    -- levelText is going to be accessed from the scene:show function. It cannot be local to
+    -- scene:create(). This is why it was declared at the top of the module so it can be seen 
+    -- everywhere in this module
     levelText = display.newText(myData.settings.currentLevel, 0, 0, native.systemFontBold, 48 )
     levelText:setFillColor( 0 )
     levelText.x = display.contentCenterX
     levelText.y = display.contentCenterY
+    --
+    -- Insert it into the scene to be managed by Composer
+    --
     sceneGroup:insert( levelText )
 
     -- 
@@ -95,10 +167,13 @@ function scene:create( event )
     -- then create the object here in scene:create()
     --
     currentScoreDisplay = display.newText("000000", display.contentWidth - 50, 10, native.systemFont, 16 )
+    sceneGroup:insert( currentScoreDisplay )
 
     --
-    -- create your objects here
+    -- these two buttons exist as a quick way to let you test
+    -- going between scenes (as well as demo widget.newButton)
     --
+
     local iWin = widget.newButton({
         label = "I Win!",
         onEvent = handleWin
@@ -117,29 +192,48 @@ function scene:create( event )
 
 end
 
+--
+-- This gets called twice, once before the scene is moved on screen and again once
+-- afterwards as a result of calling composer.gotoScene()
+--
 function scene:show( event )
+    --
+    -- Make a local reference to the scene's view for scene:show()
+    --
     local sceneGroup = self.view
 
-    params = event.params
-
+    --
+    -- event.phase == "did" happens after the scene has been transitioned on screen. 
+    -- Here is where you start up things that need to start happening, such as timers,
+    -- tranistions, physics, music playing, etc. 
+    -- In this case, resume physics by calling physics.start()
+    -- Fade out the levelText (i.e start a transition)
+    -- Start up the enemy spawning engine after the levelText fades
+    --
     if event.phase == "did" then
         physics.start()
         transition.to( levelText, { time = 500, alpha = 0 } )
         timer.performWithDelay( 500, spawnEnemies )
     else -- event.phase == "will"
-        --
-        -- reset your level here.
-        --
+        -- The "will" phase happens before the scene transitions on screen.  This is a great
+        -- place to "reset" things that might be reset, i.e. move an object back to its starting
+        -- position. Since the scene isn't on screen yet, your users won't see things "jump" to new
+        -- locations. In this case, reset the score to 0.
         currentScore = 0
         currentScoreDisplay.text = string.format( "%06d", currentScore )
     end
 end
 
+--
+-- This function gets called everytime you call composer.gotoScene() from this module.
+-- It will get called twice, once before we transition the scene off screen and once again 
+-- after the scene is off screen.
 function scene:hide( event )
     local sceneGroup = self.view
     
     if event.phase == "will" then
-        --
+        -- The "will" phase happens before the scene is transitioned off screen. Stop
+        -- anything you started elsewhere that could still be moving or triggering such as:
         -- Remove enterFrame listeners here
         -- stop timers, phsics, any audio playing
         --
@@ -149,6 +243,14 @@ function scene:hide( event )
 
 end
 
+--
+-- When you call composer.removeScene() from another module, composer will go through and
+-- remove anything created with display.* and inserted into the scene's view group for you. In
+-- many cases that's sufficent to remove your scene. 
+--
+-- But there may be somethings you loaded, like audio in scene:create() that won't be disposed for
+-- you. This is where you dispose of those things.
+-- In most cases there won't be much to do here.
 function scene:destroy( event )
     local sceneGroup = self.view
     
